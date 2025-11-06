fdece9521b509fd0b18b06ee530a5ef3662319ca
Manohar Kariyappa <manohar.kariyappa@gmail.com>
2025-11-06 08:14:25 +0000

    changes in setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
+    install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in qr_demo/__init__.py
-from bloodtestnearme import __version__ as version
+try:
+    from bloodtestnearme import __version__ as version
+except ImportError:
+    version = "0.0.1"
 
 setup(
-	name="bloodtestnearme",
-	version=version,
-	description="Demo app for printing QR codes",
-	author="ALYF GmbH",
-	author_email="hallo@alyf.de",
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
+    name="bloodtestnearme",
+    version=version,
+    description="App to manage blood tests, labs, and patient bookings",
+    author="Quantumberg Technologies Pvt Ltd",
+    author_email="admin@quantumberg.com",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires,
 )
