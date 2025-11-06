0f4a4d194d5798685a45b145c176e6e5379e511c
Manohar Kariyappa <manohar.kariyappa@gmail.com>
2025-11-06 07:29:14 +0000

    changes for qr-code py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in qr_demo/__init__.py
-from qr_demo import __version__ as version
+from bloodtestnearme import __version__ as version
 
 setup(
-	name="qr_demo",
+	name="bloodtestnearme",
 	version=version,
 	description="Demo app for printing QR codes",
 	author="ALYF GmbH",
